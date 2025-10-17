3da5a3b0ff86feb4eac045fdfc3dedbad38e5940
Filipefont26 <lipemaia00@gmail.com>
2025-10-17 20:41:24 -0300

    first commit

---
+++ ecommerce.py
@@ -0,0 +1,47 @@
+import pandas as pd 
+import matplotlib as plt
+
+#ver todas as colunas e tipos de dados 
+df  = pd.read_csv("olist_customers_dataset.csv")
+print(df.info())
+
+#aqui vemos o numero de linhas e colunas 
+print(df.shape)
+
+linhas, colunas = df.shape
+print(f"o Dataset tem {linhas} linhas e {colunas } colunas.")
+
+colunas = df.columns
+print(f"o Dataset tem as seguintes colunas :{colunas}.")
+
+#quantos clientes tem em cada estado?
+clientes_por_estado = df['customer_state']. value_counts()
+
+print(clientes_por_estado.head(10))
+
+#Quais cidades têm maior concentração de clientes?
+
+clientes_por_cidade =df['customer_city']
+print(clientes_por_cidade)
+
+#Mapemento de regiões 
+
+regioes = {
+    'AC': 'Norte', 'AP': 'Norte', 'AM': 'Norte', 'PA': 'Norte', 'RO': 'Norte', 'RR': 'Norte', 'TO': 'Norte',
+    'AL': 'Nordeste', 'BA': 'Nordeste', 'CE': 'Nordeste', 'MA': 'Nordeste', 'PB': 'Nordeste',
+    'PE': 'Nordeste', 'PI': 'Nordeste', 'RN': 'Nordeste', 'SE': 'Nordeste',
+    'DF': 'Centro-Oeste', 'GO': 'Centro-Oeste', 'MT': 'Centro-Oeste', 'MS': 'Centro-Oeste',
+    'ES': 'Sudeste', 'MG': 'Sudeste', 'RJ': 'Sudeste', 'SP': 'Sudeste',
+    'PR': 'Sul', 'RS': 'Sul', 'SC': 'Sul'}
+
+#Criar coluna de região
+df['customer_region'] = df['customer_state'].map(regioes)
+
+clientes_por_região = df['customer_region'].value_counts(normalize=True)*100
+print(f"clientes por regiao :{clientes_por_região.round(2)}")
+
+
+
+
+
+                           
